cb595a745f73f4e3befb126877067d3d83f6acbb
EC2 Default User <ec2-user@ip-172-31-3-0.ap-northeast-1.compute.internal>
2024-07-13 09:54:47 +0000

    [Add] デプロイ本番環境準備中

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -13,10 +13,21 @@ class Public::UsersController < ApplicationController
   end
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
+    if @user.update(user_params)
     redirect_to user_path(@user.id)
+    else
+       @users = User.all
+    render :edit
+    end
   end
 
   def unsubscribe
   end
+  
+    private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+  
 end
