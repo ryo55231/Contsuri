3a2b50de68483261acd76bdd979ecfe5c8d312d4
EC2 Default User <ec2-user@ip-172-31-3-0.ap-northeast-1.compute.internal>
2024-07-23 04:46:23 +0000

    [update]レイアウト微調整

--- app/controllers/user/sessions_controller.rb
+++
@@ -1,7 +0,0 @@
-class Public::Users::SessionsController < Devise::SessionsController
-  def guest_sign_in
-    user = PublicUser.guest
-    sign_in user
-    redirect_to user_path(user), notice: "ゲストでログインしました。"
-  end
-end
